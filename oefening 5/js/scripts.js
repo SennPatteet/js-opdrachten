18ac61bd85e9cd846f5faf2137b08eeea6476a53
Senn Pateet <senn.patteet@hotmail.com>
2019-01-21 11:59:33 +0100

    oefening 5

---
+++ oefening 5/js/scripts.js
@@ -0,0 +1,8 @@
+
+let firstNumber = document.getElementById('firstNumber');
+let secondNumber = document.getElementById('secondNumber');
+
+
+document.getElementById("validate").onclick = function getUserInput () {
+  alert(firstNumber.value * secondNumber.value);
+};
