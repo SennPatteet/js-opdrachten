3126fc5b2b74ea71b19c43fe0125be4d55eac014
Senn Pateet <senn.patteet@hotmail.com>
2019-01-21 10:54:04 +0100

    oefening 4

---
+++ oefening 4/js/scripts.js
@@ -0,0 +1,6 @@
+// VARIABLES -------------------------------------------------------------
+let name = prompt("What is your name?");
+let firstName = prompt("What is your first name?");
+let city = prompt("What city do you live in?");
+
+alert(`Name        : ${name}\nFirstname : ${firstName}\nCity           : ${city}`);
